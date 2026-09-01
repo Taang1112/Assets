d1c1d94ac99956765d4b8c5825c38300de2bf81a
AbbaSStar <luthfi.kirisaki.san@gmail.com>
2026-09-01 08:22:16 +0700

    update project migration karyawan

---
+++ database/migrations/2026_08_11_072020_create_karyawan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->unique();
+            $table->string('nama_lengkap');
+            $table->string('tempat_lahir')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan'])->nullable();
+            $table->string('agama')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('no_telepon')->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('status_pernikahan')->nullable();
+            $table->string('pendidikan_terakhir')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
